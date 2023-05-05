d24d8f1884efb212f7397e547fef320098451ab8
Matthew Bohush <matvei.bohush@gmail.com>
2023-05-05 19:15:42 +0200

    update firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,15 +4,27 @@ import 'firebase/auth'
 // import "firebase/storage";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDlLz6w3rLYJ_n211nRIVCK_tlV2U1GA0A',
+//   authDomain: 'handform-c62a3.firebaseapp.com',
+//   databaseURL: 'https://handform-c62a3-default-rtdb.firebaseio.com',
+//   projectId: 'handform-c62a3',
+//   storageBucket: 'handform-c62a3.appspot.com',
+//   messagingSenderId: '867248517559',
+//   appId: '1:867248517559:web:7eed23797fda6d2ae8f832',
+// }
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyDlLz6w3rLYJ_n211nRIVCK_tlV2U1GA0A',
-  authDomain: 'handform-c62a3.firebaseapp.com',
-  databaseURL: 'https://handform-c62a3-default-rtdb.firebaseio.com',
-  projectId: 'handform-c62a3',
-  storageBucket: 'handform-c62a3.appspot.com',
-  messagingSenderId: '867248517559',
-  appId: '1:867248517559:web:7eed23797fda6d2ae8f832',
+  apiKey: 'AIzaSyAcWZf_TgcSCxE3KVsKqVvlnyejtLDjxe8',
+  authDomain: 'tests-for-students-198d3.firebaseapp.com',
+  databaseURL:
+    'https://tests-for-students-198d3-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'tests-for-students-198d3',
+  storageBucket: 'tests-for-students-198d3.appspot.com',
+  messagingSenderId: '832924229661',
+  appId: '1:832924229661:web:f168d2d150b65b3322a6f4',
 }
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
